2b19535aa0e7e8c8815ecfd56d0d040acef94436
brentahrens1 <cidida@gmail.com>
2020-07-08 15:50:00 -0700

    practice with intersection observer

--- js/app.js
+++ js/app.js
@@ -8,9 +8,11 @@ let mainContainerUpcoming = document.querySelector(".main-container-upcoming");
 let upcoming = document.querySelector(".upcoming");
 let past = document.querySelector(".past");
 let open = false;
-let mainToggle = document.querySelector(".main-toggle");
+let mainToggle = document.querySelector(".main-inner");
 let mainNav = document.querySelector("nav");
-let navLink = document.querySelector(".nav-list__link");
+let navLink = document.querySelectorAll(".nav-list__link");
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".scroll")
 
 const options = {
     root: null,
@@ -23,14 +25,45 @@ const observer = new IntersectionObserver(function(entries, observer){
         if (entry.isIntersecting) {
             mainNav.classList.add("nav");
             mainNav.classList.remove("nav2");
+            navLink.forEach(link => {
+                link.classList.add("link-white");
+                link.classList.remove("link-black");
+            })
         } else {
             mainNav.classList.remove("nav");
             mainNav.classList.add("nav2");
+            navLink.forEach(link => {
+                link.classList.remove("link-white");
+                link.classList.add("link-black");
+            })
         }
     })
 },options);
 
 observer.observe(mainToggle);
+
+const scrollOptions = {
+    rootMargin: "0px 0px -50px 0px"
+}
+
+const appearonScroll = new IntersectionObserver((entries, appearonScroll) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return
+        } else {
+            entry.target.classList.add("appear");
+            appearonScroll.unobserve(entry.target);
+        }
+    })
+}, scrollOptions)
+
+faders.forEach(fader => {
+    appearonScroll.observe(fader)
+})
+
+sliders.forEach(slider => {
+    appearonScroll.observe(slider);
+})
 
 function doOpen() {
     open = !open;
